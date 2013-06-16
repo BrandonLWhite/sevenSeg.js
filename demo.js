7270b87d7ae5b559e64e87c0dda4dff3202b8d9e
Brandon White <brandonlwhite@gmail.com>
2013-06-15 22:15:35 -0500

    Split out code just for the demo into demo.js

---
+++ demo.js
@@ -0,0 +1,48 @@
+$(function () {
+    var viewModel = {
+        testVal1 : ko.observable(5),
+        testVal2 : ko.observable(12.4)
+    };
+    ko.applyBindings(viewModel);
+
+    $("#testDiv").sevenSeg();
+
+    var iValue = 0;
+    setInterval(function() {
+        $("#testDiv").sevenSeg({value: iValue});
+        if(++iValue > 9) {
+			iValue = 0;
+		}
+    }, 200);
+
+    $("#testResizableDiv").resizable();
+    $("#testSegInsideResizable").sevenSeg({value: 8});
+
+    $("#testArray").sevenSegArray({digits:3});    
+    $("#testResizableDiv2").resizable();
+    var iArrayValue = 0;
+    setInterval(function() {
+        $("#testArray").sevenSegArray({value: iArrayValue});
+        if(++iArrayValue > 999) { 
+			iArrayValue = 0; 
+		}
+    }, 50);
+
+    $("#testArray1").sevenSegArray({
+		digits:5, 
+		value:-98.76, 
+		segmentOptions: {
+			colorOff: "#003200", 
+			colorOn: "Lime", 
+			slant: 10
+		}
+	});
+    
+    $("#btnCreate").click(function() {
+        $("#testArray2").sevenSegArray({digits:4, value:12.34});
+    });
+
+    $("#btnDestroy").click(function() {
+        $("#testArray2").sevenSegArray("destroy");
+    });
+});
